ad59db50b66eb785e485cb0d889cee6c46fc0722
TJ Matelski <matelskitj@gmail.com>
2020-11-08 11:29:12 -0600

    Draw with an Element Buffer Object

--- apps/Application.cpp
+++ apps/Application.cpp
@@ -2,14 +2,24 @@
 #include <glad/glad.h>
 #include <Peanut.h>
 
+// Temporary
+#include "../src/GLDebug.h"
+
 int main()
 {
     Window window("My App", 800, 600);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
+        0.5f, 0.5f, 0.0f,   // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
 
     // Vertex Array Object
     unsigned int VAO;
@@ -26,6 +36,13 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // Element Buffer
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
     // Shader Abstraction
     Shader shader("./res/shaders/basic.shader");
     shader.Use();
@@ -38,7 +55,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Render Triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         window.SwapBuffers();
         window.PollEvents();
