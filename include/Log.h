d33cb1c2c8c07db1f5c68995340993572aae6d2c
TJ Matelski <matelskitj@gmail.com>
2021-12-22 10:38:41 -0600

    Use logging macros to get file and line number info in logs

--- include/Log.h
+++ include/Log.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#ifndef NDEBUG
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#else
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_INFO
+#endif
 #include <spdlog/spdlog.h>
 
-#define LOG_ERROR(content, ...) spdlog::error(content, ##__VA_ARGS__)
-#define LOG_INFO(content, ...) spdlog::info(content, ##__VA_ARGS__)
-#define LOG_DEBUG(content, ...) spdlog::debug(content, ##__VA_ARGS__)
+#define LOG_ERROR(content, ...) SPDLOG_ERROR(content, ##__VA_ARGS__)
+#define LOG_WARN(content, ...) SPDLOG_WARN(content, ##__VA_ARGS__)
+#define LOG_INFO(content, ...) SPDLOG_INFO(content, ##__VA_ARGS__)
+#define LOG_DEBUG(content, ...) SPDLOG_DEBUG(content, ##__VA_ARGS__)
+#define LOG_TRACE(content, ...) SPDLOG_TRACE(content, ##__VA_ARGS__)
